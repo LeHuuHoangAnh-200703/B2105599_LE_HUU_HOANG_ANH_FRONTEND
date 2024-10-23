1a7d2f3b7bd874f3dbc1e6484a23aff13ad02b6c
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2024-10-23 19:10:03 +0700

    tao trang them moi lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -8,19 +8,17 @@
         Danh bạ
         <i class="fas fa-address-book"></i>
       </h4>
-      <ContactList
-        v-if="filteredContactsCount > 0"
-        :contacts="filteredContacts"
-        v-model:activeIndex="activeIndex"
-      />
+      <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts" v-model:activeIndex="activeIndex" />
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
           <i class="fas fa-redo"></i> Làm mới
         </button>
-        <button class="btn btn-sm btn-success" @click="goToAddContact">
-          <i class="fas fa-plus"></i> Thêm mới
-        </button>
+        <router-link :to="{name : 'contact.add'}">
+          <button class="btn btn-sm btn-success">
+            <i class="fas fa-plus"></i> Thêm mới
+          </button>
+        </router-link>
         <button class="btn btn-sm btn-danger" @click="removeAllContacts">
           <i class="fas fa-trash"></i> Xóa tất cả
         </button>
@@ -33,15 +31,12 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
-        <router-link
-          :to="{
-            name: 'contact.edit',
-            params: { id: activeContact._id },
-          }"
-        >
+        <router-link :to="{
+          name: 'contact.edit',
+          params: { id: activeContact._id },
+        }">
           <span class="mt-2 badge badge-warning">
-            <i class="fas fa-edit"></i> Hiệu chỉnh</span
-          >
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span>
         </router-link>
       </div>
     </div>
@@ -118,9 +113,9 @@ export default {
       }
     },
   },
-  goToAddContact() {
-    this.$router.push({ name: "contact.add" });
-  },
+  // goToAddContact() {
+  //   this.$router.push({ name: "contact.add" });
+  // },
 
   mounted() {
     this.refreshList();
